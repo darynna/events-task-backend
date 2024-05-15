c1ade900f39a95a36ea62d33db558d8c71d313ce
Daryna Holikova <dgolikova0@gmail.com>
2024-05-15 09:59:11 +0100

    connect database

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path:
+    process.env.NODE_ENV === "production"
+      ? "./production.env"
+      : "./development.env",
+});
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+const mongoose = require("mongoose");
+const { MONGO_URL } = process.env;
+console.log(MONGO_URL);
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    app.listen(5001, () => {
+      console.log("Server running. Use our API on port: 5001");
+      console.log("Database connection successful");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
