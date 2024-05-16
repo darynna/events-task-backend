2458abb09e9d2fa210486237def6f4678692e3fe
Daryna Holikova <dgolikova0@gmail.com>
2024-05-16 15:25:12 +0100

    small changes in validaion sheme

--- utilities/validationScheme.js
+++ utilities/validationScheme.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const participantSchema = Joi.object({
   name: Joi.string().required().messages({ 'any.required': 'Participant name is required' }),
-  email: Joi.string().email().required().messages({ 'any.required': 'Participant email is required' }),
+  email: Joi.string().required().messages({ 'any.required': 'Participant email is required' }),
   dateOfBirth: Joi.string().required().messages({ 'any.required': 'Date of birth is required' }),
   heardAboutEvent: Joi.string().valid('social_media', 'friends', 'myself').required().messages({ 'any.required': 'Method of hearing about the event is required' }),
 });
